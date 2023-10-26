cfeaed7ae36afc3207eedfc228c78c86bf5d0ba5
Mich Seixas <mcseixas@gmail.com>
2023-10-26 22:25:45 +0200

    depcheck for dependencies check. Delete not used files and folders

--- test/src/services/movie.service.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-let baseUrl = import.meta.env.VITE_API_URL + "/movie";
-
-const getMovieDetails= (movieId) => {
-    return axios.get(baseUrl + '/'+ movieId)
-}
-
-
-export {getMovieDetails};
